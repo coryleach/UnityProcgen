2c87723aff089870e5f1241990414e27491aae8f
coryleach <cory.leach@gmail.com>
2020-06-19 22:54:05 -0400

    Fixes to hex mesh gen

---
+++ Runtime/Utility/HexMeshUtility.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameframe.Procgen
+{
+  public enum HexDirection
+  {
+    NE = 0, 
+    E = 1, 
+    SE = 2, 
+    SW = 3, 
+    W = 4, 
+    NW = 5
+  }
+
+  public class HexMeshData
+  {
+    public float outerRadius;
+    public float innerRadius;
+    public List<Vector3> vertices;
+    public List<int> triangles;
+    public List<Color> colors;
+    public Vector3[] corners;
+
+    private float border = 0.25f;
+    
+    public float Border => border;
+    public float Solid => 1 - border;
+
+    public HexMeshData()
+    {
+    }
+
+    public HexMeshData(float radius, float border = 0.2f)
+    {
+      this.border = Mathf.Clamp01(border);
+      
+      outerRadius = radius;
+      innerRadius = outerRadius * Mathf.Sqrt(3f) * 0.5f;
+      vertices = new List<Vector3>(); 
+      triangles = new List<int>();
+      colors = new List<Color>();
+      corners = new [] {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0f, 0f, outerRadius)
+      };
+    }
+    
+    public Vector3 GetBridge(HexDirection direction) 
+    {
+      return (corners[(int)direction] + corners[(int)direction + 1]) * 0.5f * border;
+    }
+    
+    public Vector3 GetFirstCorner(HexDirection direction)
+    {
+      return corners[(int) direction];
+    }
+
+    public Vector3 GetSecondCorner(HexDirection direction)
+    {
+      return corners[(int) direction + 1];
+    }
+    
+    public Vector3 GetFirstSolidCorner(HexDirection direction)
+    {
+      return corners[(int) direction] * Solid;
+    }
+
+    public Vector3 GetSecondSolidCorner(HexDirection direction)
+    {
+      return corners[(int) direction + 1] * Solid;
+    }
+
+    public Mesh CreateMesh()
+    {
+      Mesh mesh = new Mesh();
+      mesh.vertices = vertices.ToArray();
+      mesh.triangles = triangles.ToArray();
+      mesh.colors = colors.ToArray();
+      mesh.RecalculateNormals();
+      return mesh;
+    }
+    
+  }
+  
+  public static class HexMeshUtility
+  {
+    public static HexDirection Previous(this HexDirection direction) 
+    {
+      return direction == HexDirection.NE ? HexDirection.NW : (direction - 1);
+    }
+
+    public static HexDirection Next(this HexDirection direction) 
+    {
+      return direction == HexDirection.NW ? HexDirection.NE : (direction + 1);
+    }
+    
+    public static int GetNeighbor(int index, HexDirection hexDirection, int mapWidth, int mapHeight)
+    {
+      int y = index / mapWidth;
+      int x = index - (y * mapWidth);
+      
+      switch (hexDirection)
+      {
+        case HexDirection.NE:
+          //Only odd numbered rows shift a column 
+          if ((y & 1) == 1)
+          {
+            x++;
+          }
+          y++;
+          break;
+        case HexDirection.E:
+          x++;
+          break;
+        case HexDirection.SE:
+          //Only odd numbered rows shift a column 
+          if ((y & 1) == 1)
+          {
+            x++;
+          }
+          y--;
+          break;
+        case HexDirection.SW:
+          //Only even numbered rows shift a column 
+          if ((y & 1) == 0)
+          {
+            x--;
+          }
+          y--;
+          break;
+        case HexDirection.W:
+          x--;
+          break;
+        case HexDirection.NW:
+          //Only even numbered rows shift a column 
+          if ((y & 1) == 0)
+          {
+            x--;
+          }
+          y++;
+          break;
+        default:
+          throw new ArgumentOutOfRangeException(nameof(hexDirection), hexDirection, null);
+      }
+
+      if (x < 0 || x >= mapWidth)
+      {
+        return -1;
+      }
+
+      if (y < 0 || y >= mapHeight)
+      {
+        return -1;
+      }
+
+      return y * mapWidth + x;
+    }
+    
+    public static Mesh GenerateHexagonMesh(float radius, int startX, int startY, int chunkWidth, int chunkHeight, int mapWidth, int mapHeight, float[] heightMap, Func<float,Color> colorFunction, Func<float,float> elevationFunction)
+    {
+      var meshData = new HexMeshData(radius, 0.3f);
+
+      for (int dy = 0; dy < chunkHeight && (startY + dy) < mapHeight; dy++)
+      {
+        for (int dx = 0; dx < chunkWidth && (startX + dx) < mapWidth; dx++)
+        {
+          int x = startX + dx;
+          int y = startY + dy;
+          
+          var index = (y * mapWidth) + x;
+          
+          var xOffset = x + y * 0.5f - (int)(y / 2);
+          var center = new Vector3(xOffset*meshData.innerRadius*2,0,y*meshData.outerRadius*1.5f);
+          
+          for (var direction = 0; direction < 6; direction++)
+          {
+            var elevation = elevationFunction?.Invoke(heightMap[index]) ?? 0;
+            var previousNeighborElevation = elevation;
+            var neighborElevation = elevation;
+            var nextNeighborElevation = elevation;
+            
+            var neighbor = GetNeighbor(index, (HexDirection)direction, mapWidth, mapHeight);
+            if (neighbor != -1)
+            {
+              neighborElevation = Mathf.Min(elevation,elevationFunction?.Invoke(heightMap[neighbor]) ?? 0);
+            }
+            
+            neighbor = GetNeighbor(index, ((HexDirection)direction).Previous(), mapWidth, mapHeight);
+            if (neighbor != -1)
+            {
+              previousNeighborElevation = Mathf.Min(elevation,elevationFunction?.Invoke(heightMap[neighbor]) ?? 0);
+            }
+            
+            neighbor = GetNeighbor(index, ((HexDirection)direction).Next(), mapWidth, mapHeight);
+            if (neighbor != -1)
+            {
+              nextNeighborElevation = Mathf.Min(elevation,elevationFunction?.Invoke(heightMap[neighbor]) ?? 0);
+            }
+
+            center.y = elevation;
+            
+            var color = colorFunction?.Invoke(heightMap[index]) ?? Color.white;
+            AddTriangle(meshData, center, neighborElevation, previousNeighborElevation, nextNeighborElevation, (HexDirection)direction, color);
+          }
+
+        }
+      }
+
+      return meshData.CreateMesh();
+    }
+
+    private static void AddTriangle(HexMeshData meshData, Vector3 center, float neighborElevation, float previousElevation, float nextElevation, HexDirection direction, Color color)
+    {
+      var v1 = center;
+      var v2 = center + meshData.GetFirstSolidCorner(direction);
+      var v3 = center + meshData.GetSecondSolidCorner(direction);
+      
+      //Add inner solid triangle
+      AddTriangle(meshData, v1, v2, v3, color);
+
+      //Add Quad To Fill Border Gap
+      var v4 = v2 + meshData.GetBridge(direction);
+      v4.y = neighborElevation;
+      var v5 = v3 + meshData.GetBridge(direction);
+      v5.y = neighborElevation;
+      AddQuad(meshData, v2, v3, v4, v5, color);
+      
+      //Add Triangles to fill gap on sides of quad
+      var v6 = center + meshData.GetFirstCorner(direction);
+      v6.y = Mathf.Min(neighborElevation,previousElevation);
+      AddTriangle(meshData,v2, v6, v4, color);
+      
+      var v7 = center + meshData.GetSecondCorner(direction);
+      v7.y = Mathf.Min(neighborElevation,nextElevation);
+      AddTriangle(meshData,v3, v5, v7, color);
+    }
+
+    private static void AddTriangle(HexMeshData meshData, Vector3 v1, Vector3 v2, Vector3 v3, Color color)
+    {
+      var vertexIndex = meshData.vertices.Count;
+      
+      meshData.vertices.Add(v1);
+      meshData.vertices.Add(v2);
+      meshData.vertices.Add(v3);
+      
+      meshData.colors.Add(color);
+      meshData.colors.Add(color);
+      meshData.colors.Add(color);
+      
+      meshData.triangles.Add(vertexIndex);
+      meshData.triangles.Add(vertexIndex + 1);
+      meshData.triangles.Add(vertexIndex + 2);
+    }
+    
+    private static void AddQuad(HexMeshData meshData, Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, Color color)
+    {
+      var vertexIndex = meshData.vertices.Count;
+      
+      meshData.vertices.Add(v1);
+      meshData.vertices.Add(v2);
+      meshData.vertices.Add(v3);
+      meshData.vertices.Add(v4);
+      
+      meshData.colors.Add(color);
+      meshData.colors.Add(color);
+      meshData.colors.Add(color);
+      meshData.colors.Add(color);
+
+      meshData.triangles.Add(vertexIndex);
+      meshData.triangles.Add(vertexIndex + 2);
+      meshData.triangles.Add(vertexIndex + 1);
+      
+      meshData.triangles.Add(vertexIndex + 1);
+      meshData.triangles.Add(vertexIndex + 2);
+      meshData.triangles.Add(vertexIndex + 3);
+    }
+    
+  }
+}
+
+
--- Runtime/WorldMap/Views/WorldMapHexMeshView.cs
+++ Runtime/WorldMap/Views/WorldMapHexMeshView.cs
@@ -16,20 +16,9 @@ namespace Gameframe.Procgen
     [SerializeField] private float heightScale = 1;
     [SerializeField] private bool smooth = false;
     
+    [SerializeField] private List<WorldMapViewChunk> chunkList = new List<WorldMapViewChunk>();
     private Dictionary<Vector2, WorldMapViewChunk> _chunks = new Dictionary<Vector2, WorldMapViewChunk>();
 
-    [SerializeField] private List<WorldMapViewChunk> chunkList = new List<WorldMapViewChunk>();
-
-    public enum HexDirection
-    {
-      NE, 
-      E, 
-      SE, 
-      SW, 
-      W, 
-      NW
-    }
-    
     private void Start()
     {
       //This is here just to show the enable checkbox in editor
@@ -64,7 +53,7 @@ namespace Gameframe.Procgen
           var startY = chunkY * chunkHeight;
           var chunkView = GetChunk(new Vector2Int(chunkX,chunkY));
           chunkView.transform.localPosition = new Vector3(0,0,0);
-          var mesh = GenerateHexagonMesh(radius, startX, startY, chunkWidth, chunkHeight, mapData.width, mapData.height, heightMap, GetColor, GetElevation);
+          var mesh = HexMeshUtility.GenerateHexagonMesh(radius, startX, startY, chunkWidth, chunkHeight, mapData.width, mapData.height, heightMap, GetColor, GetElevation);
           chunkView.SetMesh(mesh);
         }
       }
@@ -136,141 +125,6 @@ namespace Gameframe.Procgen
       _chunks.Clear();
     }
 
-    public static int GetNeighbor(int index, HexDirection hexDirection, int mapWidth, int mapHeight)
-    {
-      int y = index / mapWidth;
-      int x = index - (y * mapWidth);
-      
-      switch (hexDirection)
-      {
-        case HexDirection.NE:
-          //Only odd numbered rows shift a column 
-          if ((x & 1) == 1)
-          {
-            x++;
-          }
-          y++;
-          break;
-        case HexDirection.E:
-          x++;
-          break;
-        case HexDirection.SE:
-          //Only odd numbered rows shift a column 
-          if ((x & 1) == 1)
-          {
-            x++;
-          }
-          y--;
-          break;
-        case HexDirection.SW:
-          //Only even numbered rows shift a column 
-          if ((x & 1) == 0)
-          {
-            x--;
-          }
-          y--;
-          break;
-        case HexDirection.W:
-          x--;
-          break;
-        case HexDirection.NW:
-          //Only even numbered rows shift a column 
-          if ((x & 1) == 0)
-          {
-            x--;
-          }
-          y++;
-          break;
-        default:
-          throw new ArgumentOutOfRangeException(nameof(hexDirection), hexDirection, null);
-      }
-
-      if (x < 0 || x >= mapWidth)
-      {
-        return -1;
-      }
-
-      if (y < 0 || y >= mapHeight)
-      {
-        return -1;
-      }
-
-      return y * mapWidth + x;
-    }
-    
-    public static Mesh GenerateHexagonMesh(float outerRadius, int startX, int startY, int chunkWidth, int chunkHeight, int mapWidth, int mapHeight, float[] heightMap, Func<float,Color> colorFunction, Func<float,float> elevationFunction)
-    {
-      var innerRadius = outerRadius * Mathf.Sqrt(3f) * 0.5f;
-      List<Vector3> vertices = new List<Vector3>(); 
-      List<int> triangles = new List<int>();
-      List<Color> colors = new List<Color>();
-      
-      Vector3[] corners = {
-        new Vector3(0f, 0f, outerRadius),
-        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
-        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
-        new Vector3(0f, 0f, -outerRadius),
-        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
-        new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
-      };
-
-      for (int dy = 0; dy < chunkHeight && (startY + dy) < mapHeight; dy++)
-      {
-        for (int dx = 0; dx < chunkWidth && (startX + dx) < mapWidth; dx++)
-        {
-          int x = startX + dx;
-          int y = startY + dy;
-          
-          var index = (y * mapWidth) + x;
-          float elevation = elevationFunction?.Invoke(heightMap[index]) ?? 0;
-          
-          var xOffset = x + y * 0.5f - (int)(y / 2);
-          var center = new Vector3(xOffset*innerRadius*2,elevation,y*outerRadius*1.5f);
-          for (int i = 0; i < 6; i++)
-          {
-            AddTriangle(vertices,triangles,colors,
-              center,
-              center+corners[i], 
-              center+corners[(i+1) % corners.Length], colorFunction?.Invoke(heightMap[index]) ?? Color.white);
-          }
-
-          var neighbor = GetNeighbor(index, HexDirection.E, mapWidth, mapHeight);
-          if (neighbor != -1)
-          {
-            float neighborElevation = elevationFunction?.Invoke(heightMap[neighbor]) ?? 0;
-            if (!Mathf.Approximately(neighborElevation, elevation))
-            {
-              //Draw a quad for this side
-            }
-          }
-
-        }
-      }
-      
-      Mesh mesh = new Mesh();
-      mesh.vertices = vertices.ToArray();
-      mesh.triangles = triangles.ToArray();
-      mesh.colors = colors.ToArray();
-      mesh.RecalculateNormals();
-      return mesh;
-    }
-    
-    private static void AddTriangle(List<Vector3> vertices, List<int> triangles, List<Color> colors, Vector3 v1, Vector3 v2, Vector3 v3, Color color) {
-      int vertexIndex = vertices.Count;
-      
-      vertices.Add(v1);
-      vertices.Add(v2);
-      vertices.Add(v3);
-      
-      colors.Add(color);
-      colors.Add(color);
-      colors.Add(color);
-      
-      triangles.Add(vertexIndex);
-      triangles.Add(vertexIndex + 1);
-      triangles.Add(vertexIndex + 2);
-    }
-    
   }
 
 }
